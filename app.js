83a3f35d918dea1f35e5edd3f06e42826030d7ba
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-02-27 13:46:31 -0800

    got rid of divs and open forms

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 
-var numberOfArticles
+var numberOfArticles = 5;
 console.log("hello")
 
-function search(query, number, beginYear, endYear){
+function search(query, beginYear, endYear){
 var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&begin_date=' + beginYear + '0101&sort=newest&end_date=' + endYear + '0101&api-key=f1bb266b9dff4541b8d3d390a067ccbe';
 $.ajax({
     method: "GET",
     url: nytUrl
 }).done(function (data) {
     console.log(data);
+    var results = data.response.docs;
 for (let i=0; i<numberOfArticles; i++ ) {
-
+    console.log(results[i])
 }   
 
 
@@ -38,7 +39,7 @@ $("#clear").click(function(){
     $("#beginYear").val('')
     $("#endYear").val('')
     numberOfArticles;
-    
+
 })
 
-search("trump")
+search("trump", 1999, 2018)
