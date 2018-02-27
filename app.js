65a7a583eae01cca023638910dccad21247b2409
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-02-27 13:17:31 -0800

    changed id's of search boxes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var numberOfArticles
 console.log("hello")
 
-function search(query, number, beginYear, endYear)
+function search(query, number, beginYear, endYear){
 var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&begin_date=' + beginYear + '0101&sort=newest&end_date=' + endYear + '0101&api-key=f1bb266b9dff4541b8d3d390a067ccbe';
 $.ajax({
     method: "GET",
@@ -13,13 +13,13 @@ $.ajax({
 
 
 })
+}
 
 
 $("button").click(function () {
     var query = $("#search").val().trim();
-    var beginYear = $("#search").val()
-    var endYear = $("#search").val()
-    var endYear = $("#search").val()
+    var beginYear = $("#beginYear").val()
+    var endYear = $("#endYear").val()
     number = $("#")
     search(query)
 
