32c2b1087c08297a9bd64dddee120e658399addc
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-02-27 14:20:21 -0800

    made changes

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
 
-var numberOfArticles = 5;
+var numberOfArticles 
 console.log("hello")
 
-function search(query, beginYear, endYear){
-var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&begin_date=' + beginYear + '0101&sort=newest&end_date=' + endYear + '0101&api-key=f1bb266b9dff4541b8d3d390a067ccbe';
-$.ajax({
-    method: "GET",
-    url: nytUrl
-}).done(function (data) {
-    console.log(data);
-    var results = data.response.docs;
-for (let i=0; i<numberOfArticles; i++ ) {
-    console.log(results[i])
-}   
+function search(query, beginYear, endYear, number) {
+    var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&begin_date=' + beginYear + '0101&sort=newest&end_date=' + endYear + '0101&api-key=f1bb266b9dff4541b8d3d390a067ccbe';
+    $.ajax({
+        method: "GET",
+        url: nytUrl
+    }).done(function (data) {
+        console.log(data);
+        var results = data.response.docs;
+        for (let i = 0; i < number; i++) {
+            console.log(results[i])
+            $(".panel-body").append(articleArea)
+            var articleArea = $("<div class='panel panel-default'>")
+            var articleHeader = $("<div class='panel-heading'>")
+            articleHeader.append(`<a href="${results[i].web_url}" target='_blank'>${results[i].headline.main}</a>`)
+            console.log(results[i].headline.main)
+            articleArea.append(articleHeader);
+            $(".main-body").append(articleArea)
+
+        }
 
 
-})
+    })
 }
 
 
-$("button").click(function () {
+$(document).on("click", "#submit", function () {
+    $(".panel-body").empty();
+    console.log("submit");
     var query = $("#search").val().trim();
     var beginYear = $("#beginYear").val()
     var endYear = $("#endYear").val()
-    number = $("#")
-    search(query)
+    number = numberOfArticles;
+    search(query, beginYear, endYear, number)
+
+});
 
 
-
-})
-
-$("dropdown").click(function(){
-    numberOfArticles = parseInt($(this).html());
-})
-
-$("#clear").click(function(){
-    $("#search").val("") 
+$("#clear").click(function () {
+    $("#search").val("")
     $("#beginYear").val('')
     $("#endYear").val('')
     numberOfArticles;
 
+})
+
+$('.dropdown-toggle').dropdown()
+
+$(document).on("click", ".number", function () {
+    console.log("hello22")
+    var newText = $(this).html()
+    console.log(newText)
+    numberOfArticles = parseInt($(this).html());
+    console.log(numberOfArticles)
+    $("#numberTitle").text(newText)
 })
 
 search("trump", 1999, 2018)
